7abc0d1ad0d307fd1cfd5e4adb083a53e0bb726d
mynameisjawsh <joshkeckmusic@gmail.com>
2025-08-24 12:54:02 -0500

    set up paddle functions

--- main.lua
+++ main.lua
@@ -1,15 +1,18 @@
 require("arena")
+require("paddle")
 
 function love.load()
   Arena:load()
+  Paddle:load()
 end
 
 
 function love.update(dt)
-  
+  Paddle:update(dt)
 end
 
 
 function love.draw()
   Arena:draw()
+  Paddle:draw()
 end
---
+++ paddle.lua
@@ -0,0 +1,15 @@
+Paddle = {}
+
+function Paddle:load()
+  
+end
+
+
+function Paddle:update(dt)
+  
+end
+
+
+function Paddle:draw()
+  
+end
