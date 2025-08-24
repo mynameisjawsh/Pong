eae4a87dff0a1f4a1170251e3fbb394d45984d53
mynameisjawsh <joshkeckmusic@gmail.com>
2025-08-24 13:02:31 -0500

    paddle now moves based on user input

--- paddle.lua
+++ paddle.lua
@@ -5,11 +5,16 @@ function Paddle:load()
   self.y = love.graphics.getHeight() / 2
   self.width = 20
   self.height = 100
+  self.speed = 500
 end
 
 
 function Paddle:update(dt)
-  
+  if love.keyboard.isDown("w") then
+    self.y = self.y - self.speed * dt
+  elseif love.keyboard.isDown("s") then
+    self.y = self.y + self.speed * dt
+  end
 end
 
 
