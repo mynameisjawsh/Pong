5ca29015461ffd9cb86a51978a94f4e748da8ca4
mynameisjawsh <joshkeckmusic@gmail.com>
2025-08-24 14:48:29 -0500

    wrote collision for goals

--- collisions.lua
+++ collisions.lua
@@ -16,6 +16,15 @@ function checkFloor(a, b)
 end
 
 
+function checkGoals(a, b)
+  if a.x + a.width < b.x or a.x > b.width then
+    return true
+  else
+    return false
+  end
+end
+
+
 function checkPaddle(a, b)
   if a.x + a.width > b.x and a.x < b.x + b.width and a.y + a.height > b.y and a.y < b.y + b.height then
     return true
