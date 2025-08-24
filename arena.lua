30bbff655615010f863ccb25afd5c2f9249a940f
mynameisjawsh <joshkeckmusic@gmail.com>
2025-08-24 12:51:27 -0500

    defined and drew visuals for game arena

--- arena.lua
+++ arena.lua
@@ -1,10 +1,33 @@
 Arena = {}
 
 function Arena:load()
-  
+  self.Goal = {
+    x = 0,
+    y = 0,
+    width = 30,
+    height = love.graphics.getHeight()
+  }
+  self.Divider = {
+    x = love.graphics.getWidth() / 2,
+    y = 0,
+    width = 10,
+    height = love.graphics.getHeight()
+  }
+  self.Center = {
+    x = love.graphics.getWidth() / 2,
+    y = love.graphics.getHeight() / 2,
+    radius = 100
+  }
 end
 
 
 function Arena:draw()
+  love.graphics.setColor(0, 1, 0)
+  love.graphics.rectangle("fill", self.Goal.x, self.Goal.y, self.Goal.width, self.Goal.height)
+  love.graphics.rectangle("fill", love.graphics.getWidth() - self.Goal.width, self.Goal.y, self.Goal.width, self.Goal.height)
   
+  love.graphics.setColor(0.5, 0.5, 0.5)
+  love.graphics.rectangle("fill", self.Divider.x - 5, self.Divider.y, self.Divider.width, self.Divider.height)
+  
+  love.graphics.circle("line", self.Center.x, self.Center.y, self.Center.radius)
 end
