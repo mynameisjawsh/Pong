4dbbd9602e68bf0b45902dbb56bedf59531f2942
mynameisjawsh <joshkeckmusic@gmail.com>
2025-08-24 20:46:18 -0500

    defined and drew visuals for game over state

--- gameover.lua
+++ gameover.lua
@@ -1,7 +1,14 @@
 GameOver = {}
 
 function GameOver:load()
+  self.Message = {
+    font = love.graphics.newFont(64),
+    text = "text",
+    x = 100,
+    y = 100
+  }
   
+  --buttons
 end
 
 
@@ -11,5 +18,6 @@ end
 
 
 function GameOver:draw()
-  
+  love.graphics.setFont(self.Message.font)
+  love.graphics.print(self.Message.text, self.Message.x, self.Message.y)
 end
