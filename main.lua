7ecd5b84d3fc3599ef1c6e92c0730429534683f5
mynameisjawsh <joshkeckmusic@gmail.com>
2025-08-24 19:58:41 -0500

    wrapped main logic in game state conditionals

--- main.lua
+++ main.lua
@@ -15,28 +15,34 @@ Screen.width = love.graphics.getWidth()
 Screen.height = love.graphics.getHeight()
 
 function love.load()
-  Arena:load()
-  
-  playerOne = Paddle.new(50, Screen.height / 2, 20, 100, 500)
-  playerTwo = Paddle.new(Screen.width - 70, Screen.height / 2, 20, 100, 500)
-  
-  Ball:load()
+  if GameState.play then
+    Arena:load()
+    
+    playerOne = Paddle.new(50, Screen.height / 2, 20, 100, 500)
+    playerTwo = Paddle.new(Screen.width - 70, Screen.height / 2, 20, 100, 500)
+    
+    Ball:load()
+  end
 end
 
 
 function love.update(dt)
-  playerOne:update(dt)
-  playerTwo:update(dt)
-  
-  Ball:update(dt)
+  if GameState.play then
+    playerOne:update(dt)
+    playerTwo:update(dt)
+    
+    Ball:update(dt)
+  end
 end
 
 
 function love.draw()
-  Arena:draw()
-  
-  playerOne:draw()
-  playerTwo:draw()
-  
-  Ball:draw()
+  if GameState.play then
+    Arena:draw()
+    
+    playerOne:draw()
+    playerTwo:draw()
+    
+    Ball:draw()
+  end
 end
