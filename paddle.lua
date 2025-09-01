eb8f59e316daa1aedac9e1f044deee141b9efa7b
mynameisjawsh <joshkeckmusic@gmail.com>
2025-09-01 09:47:44 -0500

    defined and drew visuals for paddle

--- paddle.lua
+++ paddle.lua
@@ -1,7 +1,10 @@
 Paddle = {}
 
 function Paddle:load()
-  
+  self.x = 50
+  self.y = love.graphics.getHeight() / 2
+  self.width = 20
+  self.height = 100
 end
 
 
@@ -11,5 +14,6 @@ end
 
 
 function Paddle:draw()
-  
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
 end
