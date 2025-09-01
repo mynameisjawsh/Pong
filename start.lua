dfabbb8a792b0af463d70d8b7bd1b8f9ac2bc9e3
mynameisjawsh <joshkeckmusic@gmail.com>
2025-09-01 14:50:02 -0500

    defined and drew visuals for game title

--- start.lua
+++ start.lua
@@ -1,7 +1,12 @@
 Start = {}
 
 function Start:load()
-  
+  Title = {
+    font = love.graphics.newFont(128),
+    text = "Pong",
+    x = Screen.width / 2,
+    y = 100
+  }
 end
 
 
@@ -11,5 +16,7 @@ end
 
 
 function Start:draw()
-  
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.setFont(Title.font)
+  love.graphics.print(Title.text, Title.x - 128, Title.y)
 end
