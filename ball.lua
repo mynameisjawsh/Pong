66a97385bf36ffc89a57f0b1138de93855db3370
mynameisjawsh <joshkeckmusic@gmail.com>
2025-09-01 10:19:19 -0500

    defined and drew visuals for ball

--- ball.lua
+++ ball.lua
@@ -1,7 +1,10 @@
 Ball = {}
 
 function Ball:load()
-  
+  self.x = Screen.width / 2
+  self.y = Screen.height / 2
+  self.width = 20
+  self.height = 20
 end
 
 
@@ -11,5 +14,5 @@ end
 
 
 function Ball:draw()
-  
+  love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
 end
