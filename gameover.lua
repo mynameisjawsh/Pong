98a64601a91376987180fff73ee090b2f5b8c95e
mynameisjawsh <joshkeckmusic@gmail.com>
2025-09-01 12:47:43 -0500

    game over screen now displays a message depending on who won

--- gameover.lua
+++ gameover.lua
@@ -1,15 +1,25 @@
 GameOver = {}
 
 function GameOver:load()
-  
+  Message = {
+    font = love.graphics.newFont(128),
+    text = " ",
+    x = 100,
+    y = 100
+  }
 end
 
 
 function GameOver:update()
-  
+  if ScoreBoard.playerOneScore > ScoreBoard.playerTwoScore then
+    Message.text = "Player One wins!"
+  else
+    Message.text = "Player Two wins!"
+  end
 end
 
 
 function GameOver:draw()
-  
+  love.graphics.setFont(Message.font)
+  love.graphics.print(Message.text, Message.x, Message.y)
 end
