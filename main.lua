da506fa0cfe707f7351c6162b7c3b8166fb05b4e
mynameisjawsh <joshkeckmusic@gmail.com>
2025-09-01 11:44:20 -0500

    created file for state handler

--- main.lua
+++ main.lua
@@ -15,25 +15,31 @@ GameState.play = true
 GameState.gameOver = false
 
 function love.load()
-  Arena:load()
-  
-  player1 = Paddle.new(50, Screen.height / 2, 20, 100, 500, 1)
-  player2 = Paddle.new(Screen.width - 70, Screen.height / 2, 20, 100, 500, 2)
-  
-  Ball:load()
+  if GameState.play then
+    Arena:load()
+    
+    player1 = Paddle.new(50, Screen.height / 2, 20, 100, 500, 1)
+    player2 = Paddle.new(Screen.width - 70, Screen.height / 2, 20, 100, 500, 2)
+    
+    Ball:load()
+  end
 end
 
 
 function love.update(dt)
-  player1:update(dt)
-  player2:update(dt)
-  Ball:update(dt)
+  if GameState.play then
+    player1:update(dt)
+    player2:update(dt)
+    Ball:update(dt)
+  end
 end
 
 
 function love.draw()
-  Arena:draw()
-  player1:draw()
-  player2:draw()
-  Ball:draw()
+  if GameState.play then
+    Arena:draw()
+    player1:draw()
+    player2:draw()
+    Ball:draw()
+  end
 end
