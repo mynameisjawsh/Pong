1df70b2c1f6fa854167d778d84543e5717ce3461
mynameisjawsh <joshkeckmusic@gmail.com>
2025-09-01 10:22:14 -0500

    defined collision for ball on paddle

--- collisions.lua
+++ collisions.lua
@@ -14,3 +14,12 @@ function checkFloor(a, b)
     return false
   end
 end
+
+
+function checkPaddle(a, b)
+  if a.x + a.width > b.x and a.x < b.x + b.width and a.y + a.height > b.y and a.y < b.y + b.height then
+    return true
+  else
+    return false
+  end
+end
