ee99eba9d7b6c54f71ddb08ef7b5967a19fb860c
mynameisjawsh <joshkeckmusic@gmail.com>
2025-09-02 10:24:44 -0500

    defined and drew visuals for mode selection screen

--- modeselect.lua
+++ modeselect.lua
@@ -1,7 +1,12 @@
 ModeSelect = {}
 
 function ModeSelect:load()
-  
+  Message = {
+    font = love.graphics.newFont(128),
+    text = "Select Mode:",
+    x = 100,
+    y = 100
+  }
 end
 
 
@@ -11,5 +16,7 @@ end
 
 
 function ModeSelect:draw()
-  
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.setFont(Message.font)
+  love.graphics.print(Message.text, Message.x, Message.y)
 end
