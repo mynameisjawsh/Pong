0c3a294c2019c51c461bd84d0cc82e0e0141804b
mynameisjawsh <joshkeckmusic@gmail.com>
2025-09-02 12:34:40 -0500

    defined sounds as variables

--- sound.lua
+++ sound.lua
@@ -1,7 +1,11 @@
 Sound = {}
 
 function Sound:load()
-  
+  self.HitPaddle1 = love.audio.newSource("Assets/Sounds/Exports/HitPaddle1.wav", "static")
+  self.HitPaddle2 = love.audio.newSource("Assets/Sounds/Exports/HitPaddle2.wav", "static")
+  self.HitCeiling = love.audio.newSource("Assets/Sounds/Exports/HitCeiling.wav", "static")
+  self.HitFloor = love.audio.newSource("Assets/Sounds/Exports/HitFloor.wav", "static")
+  self.ClickButton = love.audio.newSource("Assets/Sounds/Exports/ClickButton.wav", "static")
 end
 
 
