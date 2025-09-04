440534321e0c75603e9d6ad96f051c5011354d6d
mynameisjawsh <joshkeckmusic@gmail.com>
2025-09-04 16:51:42 -0500

    defined and drew visuals for single player ai

--- singleplayerai.lua
+++ singleplayerai.lua
@@ -1,7 +1,11 @@
 SinglePlayerAI = {}
 
 function SinglePlayerAI:load()
-  
+  self.x = Screen.width - 70
+  self.y = Screen.height / 2
+  self.width = 20
+  self.height = 100
+  self.speed = 500
 end
 
 
@@ -11,5 +15,6 @@ end
 
 
 function SinglePlayerAI:draw()
-  
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
 end
