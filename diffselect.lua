9b115df46b105fd7ca1acec050f8b0f13b7d1df0
mynameisjawsh <joshkeckmusic@gmail.com>
2025-09-05 16:59:50 -0500

    defined and drew text for difficulty selection screen

--- diffselect.lua
+++ diffselect.lua
@@ -1,7 +1,12 @@
 DifficultySelect = {}
 
 function DifficultySelect:load()
-  
+  Message = {
+    font = love.graphics.newFont(128),
+    text = "Select Difficulty:",
+    x = 100,
+    y = 100
+  }
 end
 
 
@@ -11,5 +16,7 @@ end
 
 
 function DifficultySelect:draw()
-  
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.setFont(Message.font)
+  love.graphics.print(Message.text, Message.x, Message.y)
 end
